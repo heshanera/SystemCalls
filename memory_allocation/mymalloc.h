aa104b95e672b90336f19300e5a58c1c670c3f63
Heshan Eranga <erangamx@gmail.com>
2016-11-23 22:17:12 +0530

    Update mymalloc.h

--- memory_allocation/mymalloc.h
+++ memory_allocation/mymalloc.h
@@ -1,7 +1,28 @@
 #include <stdio.h>
 
-int memory[500];
+char memory[500];
 
-// 
-
-
+char * MyMalloc(int size)
+{
+	
+	//printf("%d",size);
+	/**
+	 * increasing the filled size
+	 */
+	memory[499] += size;
+	
+	/**
+	 * increasing the pointer that points to the first unallocated
+	 * memory in the array
+	 */ 
+	memory[498] += size;
+	
+	//printf("%d ******* \n",( memory[498] - size));
+	
+	*( memory + memory[498] ) = size;
+	
+	/**
+	 * returning the pointer
+	 */ 
+	return (memory + ( memory[498] - size) + 1);
+}
